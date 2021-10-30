0465c82adc48f62a14b336825662c2e171046c8e
kunalsatija009 <83098182+kunalsatija009@users.noreply.github.com>
2021-10-29 21:42:23 -0400

    Update GithubAPI.py

--- GithubAPI.py
+++ GithubAPI.py
@@ -1,21 +1,22 @@
+import unittest
 import requests
 import json
 
-def commit_count(userID):
-    """This Function will count the number of commits"""
-    repos =  get_repository(userID)
-    dict = {}
-    for entry in repos:
-        name = entry['name']
-        dict[name] = get_commits(userID, name)
-    return dict
-        
-def  get_repository(userID):
-    """This function will get the repository of the userid entered"""
-    getRepo = requests.get(f"https://api.github.com/users/{userID}/repos").json()
-    return getRepo
 
-def get_commits(userID, name):
-    """This function will get the commits of the userid entered"""
-    commitURL = requests.get(f"https://api.github.com/repos/{userID}/{name}/commits").json()
-    return len(commitURL)
+ 
+def git_data(userid):
+    """Given the Github userid and return name of repo and number of commits"""
+    commit_count = 0
+    dict = {}
+    get_repository = requests.get(f"https://api.github.com/users/{userid}/repos")
+    repository_data =get_repository.json()
+    for entry in repository_data:
+        name = entry['name']
+
+        get_commits = requests.get(f"https://api.github.com/repos/{userid}/{name}/commits")
+        commit_data = get_commits.json()
+        for entry in commit_data:
+            if entry["commit"]:
+                commit_count +=1
+        dict[name] = commit_count
+    return dict
